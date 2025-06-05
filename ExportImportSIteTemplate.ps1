db203e282040851977ad76c7f8fa7a9051898ccd
Arunbhai <102005162+Arun09012003@users.noreply.github.com>
2025-06-05 10:05:57 +0530

    Update ExportImportSIteTemplate.ps1

--- ExportImportSIteTemplate.ps1
+++ ExportImportSIteTemplate.ps1
@@ -1,12 +1,7 @@
-$ClientId = "d5aff94a-8d5e-4f4c-99f5-4abaec89dd7f"
-$TenantId = "dgneaseteq.onmicrosoft.com"
+$ClientId = "< client id >"
+$TenantId = "< tenant.onmicrosoft.com >"
 
-Connect-PnPOnline -Url "https://dgneaseteq.sharepoint.com/sites/7777789" -ClientId $ClientId -Tenant $TenantId -Interactive 
-Get-PnPSiteTemplate -Configuration "C:\Projects\PnP Powershell\Project\ExtractConfigurationSchemaForPages.JSON" -Out "C:\Projects\PnP Powershell\Project\SiteTemplate\TeamSiteStructure.xml"
+Connect-PnPOnline -Url "< site url >" -ClientId $ClientId -Tenant $TenantId -Interactive 
+Get-PnPSiteTemplate -Configuration "< configuration file >" -Out "< file path to store xml template structure >"
 
 
-
-# Get-PnPSiteTemplate -Out "C:\Projects\PnP Powershell\Project\SiteTemplate\ArunSiteTemplate.xml"
-
-# Connect-PnPOnline -Url "https://dgneaseteq.sharepoint.com/sites/ISIte" -ClientId $ClientId -Tenant $TenantId -Interactive 
-# Invoke-PnPSiteTemplate -Path "C:\Projects\PnP Powershell\Project\SiteTemplate\ArunSiteTemplate.xml"
