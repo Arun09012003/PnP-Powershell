a43ba8e8533b7bbd6e87d9d25b93f7125e94fc76
Arunbhai <102005162+Arun09012003@users.noreply.github.com>
2025-06-05 10:07:13 +0530

    Update SharePointConnection-CertificateThumbprint.ps1

--- SharePointConnection-CertificateThumbprint.ps1
+++ SharePointConnection-CertificateThumbprint.ps1
@@ -1,13 +1,13 @@
 # Register app on azure with certificate files .pfx & .cer files.
-Register-PnPAzureADApp -ApplicationName "App_Name" -Tenant "dgneaseteq.onmicrosoft.com" -Store CurrentUser -SharePointApplicationPermissions "Sites.FullControl.All"
+Register-PnPAzureADApp -ApplicationName "< appName >" -Tenant "tenant.onmicrosoft.com" -Store CurrentUser -SharePointApplicationPermissions "Sites.FullControl.All"
 
 # Connection using certificate thumbprint.
-$BaseURL = "https://dgneaseteq.sharepoint.com"
+$BaseURL = "https://tenant.sharepoint.com"
 
-$ClientID = "293f06c2-8425-47de-b12d-1c78cd3f6a0d"
+$ClientID = "< client id >"
 
-$ThumbPrint = "04D06A24DEB15CA77749A42ADFE4A10B10066D0E"
+$ThumbPrint = "< certificate thumbprint >"
 
-$Tenant = "dgneaseteq.onmicrosoft.com"
+$Tenant = "tenant.onmicrosoft.com"
  
 Connect-PnPOnline -Url "$BaseURL" -ClientId $ClientID -Thumbprint $ThumbPrint -Tenant $Tenant
