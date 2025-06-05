a59310c7902d847bde51a57d7676d1a6c22e2e4e
Arunbhai <102005162+Arun09012003@users.noreply.github.com>
2025-06-05 10:13:41 +0530

    Update CreateTeamSiteWithGroup.ps1

--- CreateTeamSiteWithGroup.ps1
+++ CreateTeamSiteWithGroup.ps1
@@ -4,7 +4,7 @@ $Tenant = "tenant.onmicrosoft.com"
 $CertificatePath = "< .pfx file path >"
 $Password = (ConvertTo-SecureString -AsPlainText "< certificate password >" -Force)
 
-Connect-PnPOnline -ClientId $ClientID -CertificatePath $CertificatePath -CertificatePassword $Password -Url "$BaseURL/Arun" -Tenant $Tenant
+Connect-PnPOnline -ClientId $ClientID -CertificatePath $CertificatePath -CertificatePassword $Password -Url "<site url>" -Tenant $Tenant
 
 # If connection is CertificatePath then use this
 New-PnPSite -Type TeamSite `
