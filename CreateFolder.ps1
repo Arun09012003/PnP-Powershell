89262e99b0dfed45e2b15d36d6c1c7bd7f9d021b
Arunbhai <102005162+Arun09012003@users.noreply.github.com>
2025-06-05 10:12:07 +0530

    Update CreateFolder.ps1

--- CreateFolder.ps1
+++ CreateFolder.ps1
@@ -1,8 +1,8 @@
 # Create folder in sharepoint document library using PnPPowershell.
 
-$ClientId = "d5aff94a-8d5e-4f4c-99f5-4abaec89dd7f"
-$TenantId = "dgneaseteq.onmicrosoft.com"
-Connect-PnPOnline -Url "https://dgneaseteq.sharepoint.com/sites/100102" -ClientId $ClientId -Tenant $TenantId -Interactive
+$ClientId = "client id"
+$TenantId = "<tenant.onmicrosoft.com>"
+Connect-PnPOnline -Url "<site url>" -ClientId $ClientId -Tenant $TenantId -Interactive
 $folderNames = @("Client-1001", "Client-1002", "Client-1003")
 $libraryName = "Shared Documents"
 foreach ($name in $folderNames) {
